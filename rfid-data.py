02225a06711fc5b91ccd09cc177f45b4e22fe19b
Venukanthan BS <114436303+itsvenu22@users.noreply.github.com>
2023-06-04 17:18:46 +0530

    Adding exceptions
    Adding exceptions to catch 'AUTH' and 'Error while adding'

--- rfid-data.py
+++ rfid-data.py
@@ -8,12 +8,28 @@ max_tries = 3
 try_count = 0
 
 reader = SimpleMFRC522()
-while try_count < max_tries:
-    try:
-        id, data = reader.read()
-        a = str(id)
-        print(a)
-        print(data)
-        break
-    finally:
-        GPIO.cleanup()
+try:
+    while try_count < max_tries:
+        try:
+            id, data = reader.read()
+            a = str(id)
+            print(a)
+            print(data)
+            break
+        except Exception as e:
+            if str(e) == "Error while reading!":
+                try_count += 1
+                print(f"Retry {try_count}/{max_tries}")
+            elif str(e) == "AUTH ERROR" or "AUTH ERROR!!":
+                print("Authentication error. Please check authentication keys.")
+                break
+            else:
+                print(f"Error: {e}")
+                break
+finally:
+    GPIO.cleanup()
+
+
+
+
+
