ad3452c1b66a37b2dea26797773dc26ffa23c67a
Venukanthan BS <114436303+itsvenu22@users.noreply.github.com>
2023-06-04 16:12:28 +0530

    Added a loop with max tries
    Added a loop with max tries to avoid 'AUTH' and 'Error while reading' errors

--- rfid-data.py
+++ rfid-data.py
@@ -4,17 +4,16 @@ from mfrc522 import SimpleMFRC522
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
+max_tries = 3
+try_count = 0
+
 reader = SimpleMFRC522()
-try:
-    id, data = reader.read()
-    a = str(id)
-    print(a)
-    print(data)
-
-finally:
-    GPIO.cleanup()
-
-
-
-
-
+while try_count < max_tries:
+    try:
+        id, data = reader.read()
+        a = str(id)
+        print(a)
+        print(data)
+        break
+    finally:
+        GPIO.cleanup()
