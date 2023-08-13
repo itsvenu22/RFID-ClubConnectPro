f521830e0d1c5a1e8a6f89d69cab979bae6b5705
Venukanthan BS <114436303+itsvenu22@users.noreply.github.com>
2023-08-13 11:55:30 +0530

    Update rfid-data.py

--- rfid-data.py
+++ rfid-data.py
@@ -7,33 +7,6 @@ from mfrc522 import SimpleMFRC522
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def send_email(sender_email, sender_password, receiver_email, subject, message):
-
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
-    msg['Subject'] = subject
-
-    msg.attach(MIMEText(message, 'plain'))
-
-    try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print("Email sent successfully!")
-
-    except Exception as e:
-        print("An error occurred while sending the email:", str(e))
-
-    finally:
-        server.quit()
-
-def send_lug_message(mess,send_num,receive_num):
-    client = Client(account_sid, auth_token)
-    message = client.messages.create(from_=send_num,body=mess,to=receive_num)
-    print(message.sid)
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 
